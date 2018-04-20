289098ea8400751a4070441533bad9db982c96a3
David <divadg94@gmail.com>
2018-04-20 19:18:50 -0400

    april20

--- hw/11hw/cvt.py
+++ hw/11hw/cvt.py
@@ -4,12 +4,12 @@ import os
 import sys
 import time
 
-Nlins=[50,100,150]
-Betas=np.flip(np.arange(0.2,0.8,0.025),0)
+Nlins=[75]
+Betas=np.flip(np.arange(0.25,0.7,0.01),0)
 Temps=1/Betas
 Nbs=100 #number of botstrap tests
 Neql=1000
-Nbin=1000
+Nbin=10000
 Nmcs=20
 SEED=10
 method='sqlatt_PBC'
@@ -20,13 +20,13 @@ def cvfunc(data,Ns,T):
   return Ns/(T*T)*(e2-e*e)
 
 for i in range(len(Nlins)):
-  print('Nlin='+str(Nlins[i]))
+  #print('Nlin='+str(Nlins[i]))
   Ns=Nlins[i]*Nlins[i]
   cvvals=np.zeros(len(Temps))
   cverrs=np.zeros(len(Temps))
   #now run the code once for each temperature
   for j in range(len(Temps)):
-    print('\tBeta='+str(Betas[j]))
+    #print('\tBeta='+str(Betas[j]))
     pfile=open('param.dat','w')
     pfile.write('%d %f %d %d %ld %d %s\n' %(Nlins[i],1.0/Temps[j],Neql,Nmcs,Nbin,SEED,method))
     pfile.close()
@@ -41,14 +41,16 @@ for i in range(len(Nlins)):
       tempcv[k]=cvfunc(randdata,Ns,Temps[j])
     #use original data for mean
     cverrs[j]=np.std(tempcv)
+    print(str(Nlins[i])+' '+str(Betas[j])+' '+str(cvvals[j])+' '+str(cverrs[j]))
   label='L='+str(Nlins[i])
   normalize=np.max(cvvals)
   cvvals=cvvals/normalize
   cverrs=cverrs/normalize
+'''
   plt.errorbar(Betas,cvvals,yerr=cverrs,label=label)
 plt.legend(loc='upper right')
 plt.xlim(0.9,0.1)
 plt.xlabel('Beta')
 plt.ylabel('Specific Heat')
 plt.show()
-
+'''
